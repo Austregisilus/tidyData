16dfe8981ba380dadaf2580a08d715f098142d19
Austregisilus <87029009+Austregisilus@users.noreply.github.com>
2021-09-14 23:47:18 +0300

    Add files via upload

---
+++ run_anaylsis.R
@@ -0,0 +1,25 @@
+X <- rbind(x_train, x_test)
+Y <- rbind(y_train, y_test)
+Subject <- rbind(subject_train, subject_test)
+Merge_data <- cbind(Subject, X, Y)
+
+tidyData <- select(Merge_data, subject, code, contains("mean"), contains("std"))
+
+names(tidyData)[2] = "activity"
+names(tidyData)<-gsub("Acc", "Accelerometer", names(tidyData))
+names(tidyData)<-gsub("Gyro", "Gyroscope", names(tidyData))
+names(tidyData)<-gsub("BodyBody", "Body", names(tidyData))
+names(tidyData)<-gsub("Mag", "Magnitude", names(tidyData))
+names(tidyData)<-gsub("^t", "Time", names(tidyData))
+names(tidyData)<-gsub("^f", "Frequency", names(tidyData))
+names(tidyData)<-gsub("tBody", "TimeBody", names(tidyData))
+names(tidyData)<-gsub("-mean()", "Mean", names(tidyData), ignore.case = TRUE)
+names(tidyData)<-gsub("-std()", "STD", names(tidyData), ignore.case = TRUE)
+names(tidyData)<-gsub("-freq()", "Frequency", names(tidyData), ignore.case = TRUE)
+names(tidyData)<-gsub("angle", "Angle", names(tidyData))
+names(tidyData)<-gsub("gravity", "Gravity", names(tidyData))
+
+finalData <- tidyData %>% group_by(subject, activity) %>% summarise_all(funs(mean))
+
+write.table(finalData, "finalData.txt", row.name=FALSE)
+
