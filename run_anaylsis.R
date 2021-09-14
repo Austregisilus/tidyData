294a148df6373843ca1453629d15ed9ba6098375
Austregisilus <87029009+Austregisilus@users.noreply.github.com>
2021-09-15 00:05:37 +0300

    Add files via upload

--- run_anaylsis.R
+++ run_anaylsis.R
@@ -1,10 +1,23 @@
+# Read the files
+features <- read.table("./data/UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("./data/UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+# Merge the files
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
 Subject <- rbind(subject_train, subject_test)
 Merge_data <- cbind(Subject, X, Y)
 
+# Make the data tidy
 tidyData <- select(Merge_data, subject, code, contains("mean"), contains("std"))
 
+# Rename column name or headers
 names(tidyData)[2] = "activity"
 names(tidyData)<-gsub("Acc", "Accelerometer", names(tidyData))
 names(tidyData)<-gsub("Gyro", "Gyroscope", names(tidyData))
@@ -19,7 +32,9 @@ names(tidyData)<-gsub("-freq()", "Frequency", names(tidyData), ignore.case = TRU
 names(tidyData)<-gsub("angle", "Angle", names(tidyData))
 names(tidyData)<-gsub("gravity", "Gravity", names(tidyData))
 
+# Create final data
 finalData <- tidyData %>% group_by(subject, activity) %>% summarise_all(funs(mean))
 
+# Extract the final data
 write.table(finalData, "finalData.txt", row.name=FALSE)
 
